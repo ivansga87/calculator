dc929ec24d939ab894b0eda2c17581bcbf1186c9
ivans <249990452+ivansga87@users.noreply.github.com>
2026-03-06 22:39:32 -0600

    implemented . succesfully... I think, was cleaning up code and broke the operator chaning but I think all I have to do is work on updating the current operator and next operator in my funtions

--- calculator.js
+++ calculator.js
@@ -1,157 +1,132 @@
-function addition(a, b) {
-    return a + b;
-}
 
-function substraction(a, b) {
-    return a - b;
-}
-
-function multiplication(a, b) {
-    return a * b;
-}
-
-function division(a, b) {
-    if (b === 0) {
-        return "cant divede by 0";
-    }
-    return a / b;
-}
 
 let a = "";
 let b = "";
 let currentOperator = "";
-let resetDisplay = false
-
-
-const para = document.getElementById("display")
+let resetDisplay = false;
+let period = false;
+const para = document.getElementById("display1")
 para.textContent = "";
+
+
 
 const buttons = document.getElementById("buttons-container")
 buttons.addEventListener("click", function (e) {
-
+    const buttonValue = e.target.textContent;
     if (e.target.id === "clear") {
         para.textContent = "";
         a = "";
         b = "";
         currentOperator = "";
+        period = false;
     }
     else if (e.target.classList.contains("number")) {
         if (resetDisplay) {
-            para.textContent = "";
-            resetDisplay = false
+            para.textContent = buttonValue;
+            resetDisplay = false;
         }
-        const buttonValue = e.target.textContent;
-        para.textContent += buttonValue;
+
+        else para.textContent += buttonValue;
     }
     else if (e.target.id === "multiplication") {
-                
         if (a !== "") {
-            b = para.textContent;
-            const result = runCalc(currentOperator);
-            para.textContent = Number(result.toFixed(4));
-            a = result;
-            b = "";
-            currentOperator = "x"
-            resetDisplay = true;
+            runCalculation(currentOperator)
 
         }
         else {
-            a = para.textContent
-            para.textContent = "";
-            currentOperator = "x";
+           setOperator("x")
         }
 
     }
     else if (e.target.id === "division") {
-        if (a !== "") {
-            b = para.textContent;
-            const result = runCalc(currentOperator);
-            para.textContent = Number(result.toFixed(4));
-            a = result;
-            b = "";
-            currentOperator = "/"
-            resetDisplay = true;
-        }
-
-        else {
-            a = para.textContent
-            para.textContent = "";
-            currentOperator = "/";
-        }
-
+        if (a !== "") runCalculation(currentOperator)
+        
+        else setOperator("/")
     }
     else if (e.target.id === "addition") {
         if (a !== "") {
-            b = para.textContent;
-            const result = runCalc(currentOperator);
-            para.textContent = Number(result.toFixed(4));
-            a = result;
-            b = "";
-            currentOperator = "+"
-            resetDisplay = true;
+            runCalculation(currentOperator);
+
         }
 
         else {
-            a = para.textContent
-            para.textContent = "";
-            currentOperator = "+";
+           setOperator("+")
         }
     }
     else if (e.target.id === "substraction") {
         if (a !== "") {
-            b = para.textContent;
-            const result = runCalc(currentOperator);
-            para.textContent = Number(result.toFixed(4));
-            a = result;
-            b = "";
-            currentOperator = "-"
-            resetDisplay = true;
+            runCalculation(currentOperator)
         }
 
         else {
-            a = para.textContent
-            para.textContent = "";
-            currentOperator = "-";
+            setOperator("-")
         }
 
     }
-
     else if (e.target.id === "equal") {
-       
-        
         b = para.textContent;
         const result = runCalc(currentOperator);
         para.textContent = Number(result.toFixed(4));
-
         a = "";
         b = "";
         currentOperator = "";
         resetDisplay = true;
-        
+        period = false;
+    }
+    else if (e.target.id === "period") {
+        if (resetDisplay) {
+            para.textContent = buttonValue;
+            resetDisplay = false;
+        }
+        else if (period) return;
+
+
+        else {
+            para.textContent += buttonValue
+            period = true
+        }
+
 
     }
-
 }
 
 )
 
-function runCalc(currentOperator) {
-    let result = 0;
 
-    if (currentOperator === "x") {
-        result = multiplication(Number(a), Number(b));
-
-    }
-    else if (currentOperator === "/") {
-        result = division(Number(a), Number(b));
-    }
-    else if (currentOperator === "+") {
-        result = addition(Number(a), Number(b));
-    }
-    else if (currentOperator === "-") {
-        result = substraction(Number(a), Number(b));
-    }
-    return result
+function runCalculation(currentOperator) {
+    currentOperator = currentOperator;
+    b = para.textContent;
+    const result = runCalc(currentOperator);
+    para.textContent = Number(result.toFixed(4));
+    a = result;
+    b = "";
+    resetDisplay = true;
+    period = false;
 
 }
 
+function runCalc(currentOperator) {
+    if (currentOperator === "x") return multiplication(Number(a), Number(b));
+    if (currentOperator === "/") return division(Number(a), Number(b));
+    if (currentOperator === "+") return addition(Number(a), Number(b));
+    if (currentOperator === "-") return substraction(Number(a), Number(b));
+    return 0;
+}
+
+function addition(a, b){return a + b;}
+
+function substraction(a, b) {return a - b;}
+
+function multiplication(a, b) {return a * b;}
+
+function division(a, b) {
+    if (b === 0) return "cant divede by 0";
+    return a / b;
+}
+
+function setOperator(opSymbol) {
+    a = para.textContent
+    currentOperator = opSymbol;
+    period = false;
+    resetDisplay = true;
+}
